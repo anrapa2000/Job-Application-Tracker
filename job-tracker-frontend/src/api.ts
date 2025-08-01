52c0c4bd84c979867c8dd07dd866cf13801220f2
Aparna Chandramouli <caparna2000@gmail.com>
2025-08-01 15:58:04 -0700

    Deployed URL changes

--- job-tracker-frontend/src/api.ts
+++ job-tracker-frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://job-application-tracker-2mm8.onrender.com',
 });
 
 export default api;
